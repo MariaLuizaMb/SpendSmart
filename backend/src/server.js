71c45c9e65cb770aedb35ace604ccf27d2daca10
Maria Luiza Madeira <mariamadeirabm2@gmail.com>
2026-04-27 13:03:08 -0300

    chore: adiciona configuração inicial do Docker
    Configura Dockerfiles para backend e frontend, adiciona arquivos .dockerignore e define docker-compose para executar os serviços da aplicação em containers.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import "dotenv/config.js";
 import app from "./app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
