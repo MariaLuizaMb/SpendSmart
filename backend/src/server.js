80a23e1ea895dd42bf0f2839ab7caaf49afab2a2
Maria Luiza Madeira <mariamadeirabm2@gmail.com>
2026-07-27 00:25:28 -0300

    feat: adicionar verificação de chave de API e melhorias na configuração do Docker

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
 import "dotenv/config.js";
 import app from "./app.js";
+import { startFinancialAnalysisWorker } from "./jobs/workers/financialAnalysisWorker.js";
+import { startBudgetAlertWorker } from "./jobs/workers/budgetAlertWorker.js";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+startFinancialAnalysisWorker();
+startBudgetAlertWorker();
